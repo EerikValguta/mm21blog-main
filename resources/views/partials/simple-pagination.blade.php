2998f225ee24de59f958d6a6e1fd1652d613996b
Eerik <eerik.valguta@gmail.com>
2023-12-19 12:03:58 +0200

    uus

--- resources/views/partials/simple-pagination.blade.php
+++ resources/views/partials/simple-pagination.blade.php
@@ -1,24 +1,18 @@
-<style>
-    .pagination {
-        display: flex;
-    }
-</style>
-
 @if ($paginator->hasPages())
     <nav>
         <ul class="pagination">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
+                <li class="disabled" aria-disabled="true"><button class="btn btn-disabled" disabled="disabled">@lang('pagination.previous')</button></li>
             @else
                 <li><a class="join-item btn btn-primary" href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
             @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li><a class="join-item btn btn-primary"href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+                <li><a class="join-item btn btn-primary" href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
             @else
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
+                <li class="disabled" aria-disabled="true"><button class="btn btn-disabled" tabindex="-1" role="button" aria-disabled="true">@lang('pagination.next')</button></li>
             @endif
         </ul>
     </nav>
